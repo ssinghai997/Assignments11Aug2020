c0fb5d1cb3c875a87eea34279dcfc097e26669d3
ssinghai997 <68274724+ssinghai997@users.noreply.github.com>
2020-08-11 19:20:16 +0530

    Add Calc Lib

---
+++ CalcLib/CalcLib/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLib
+{
+    
+    public class CalcClass
+    {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+
+
+        public double Add()
+        {
+            return num1 + num2;
+        }
+        public double Sub()
+        {
+            return num1 - num2;
+        }
+        public double Mul()
+        {
+            return num1 * num2;
+        }
+        public double Div()
+        {
+            return num1 / num2;
+        }
+    }
+}
